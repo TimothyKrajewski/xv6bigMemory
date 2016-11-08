26b568f23c9fbdb7e229f2aefe91cb948b46e86c
TimothyKrajewski <krajewt1@tcnj.edu>
2016-11-08 07:17:56 -0700

    Sanity is working THANK GOD ready for testing

--- defs.h
+++ defs.h
@@ -117,6 +117,7 @@ int             wait(void);
 void            wakeup(void*);
 void            yield(void);
 int 		ps(void);
+int 		wait2(int*,int*,int*);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
--- sysproc.c
+++ sysproc.c
@@ -101,10 +101,14 @@ yield();
 return 0;
 }
 
-int sys_wait2(void)
-{
-	int *retime, *rutime, *stime;
-	if(argptr(2,(void*)&stime, sizeof(stime) < 0)
-		return -1;
-	return wait2(retime,rutime,stime);
+int sys_wait2(void) {
+  int *retime, *rutime, *stime;
+  if (argptr(0, (void*)&retime, sizeof(retime)) < 0)
+    return -1;
+  if (argptr(1, (void*)&rutime, sizeof(retime)) < 0)
+    return -1;
+  if (argptr(2, (void*)&stime, sizeof(stime)) < 0)
+    return -1;
+  return wait2(retime, rutime, stime);
 }
+
